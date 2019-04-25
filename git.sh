73a02a3b50593ee91791efec7b96ae439c5b10c8
Parnikkapore <9170353+parnikkapore@users.noreply.github.com>
2019-04-25 16:47:06 +0700

    Made README a Markdown file like it should be, and added the git helper script again. Protip: run `. git.sh` then `git_save_upload` to push local changes

---
+++ git.sh
@@ -0,0 +1,9 @@
+# Convenience functions for Git
+
+function git_save(){
+    git add -A && git commit -a
+}
+
+function git_save_upload(){
+    git add -A && git commit -a && git push -u origin master
+}
